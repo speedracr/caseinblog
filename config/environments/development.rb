cfa28a35ef6cc9a654afc536b550cab66b909a7b
Jonas Knipper <jonas@robocomobo.com>
2015-12-13 18:44:11 +0100

    Set up Mandrill SMTP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,13 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrillapp.com",
+    port: 25,
+    user_name: ENV["MANDRILL_USERNAME"],
+    password: ENV["MANDRILL_API_KEY"]
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
